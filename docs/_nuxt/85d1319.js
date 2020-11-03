f6b0d6ba20d15d2615f9e7accc4039ac180735ec
Nikita Morozov <morozov.space@mail.ru>
2020-11-03 09:47:02 +0300

    first build

---
+++ docs/_nuxt/85d1319.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{192:function(e,n,t){"use strict";t.r(n);var l=t(18),component=Object(l.a)({},(function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("Home page")])}),[],!1,null,null,null);n.default=component.exports}}]);
