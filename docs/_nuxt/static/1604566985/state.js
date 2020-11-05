fa71351bfbe114099dd6010e8b0bfdac296ae182
root <root@DESKTOP-S48R5QP.localdomain>
2020-11-05 12:03:42 +0300

    build 6

---
+++ docs/_nuxt/static/1604566985/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1604566985",layout:"default",error:null,state:{participant:b,loaders:{default:{status:a,message:a,hidden:b},camera:{status:a,message:a,hidden:c}}},serverRendered:c,routePath:"\u002F",config:{}}}("loading",false,true));
