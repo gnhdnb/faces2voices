0abcc18bd8a32583607f89daa07af2293fee4198
Nikita Morozov <morozov.space@mail.ru>
2020-11-05 12:02:50 +0300

    build 4

--- docs/_nuxt/static/1604564813/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1604564813",layout:"default",error:null,state:{participant:b,loaders:{default:{status:a,message:a,hidden:b},camera:{status:a,message:a,hidden:c}}},serverRendered:c,routePath:"\u002F",config:{}}}("loading",false,true));
