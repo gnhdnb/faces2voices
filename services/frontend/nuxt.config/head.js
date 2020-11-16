0367da288fa30e62859af781cfe5c631cb5256c1
Nikita Morozov <morozov.space@mail.ru>
2020-11-16 15:49:05 +0300

    add links in head.js

--- services/frontend/nuxt.config/head.js
+++ services/frontend/nuxt.config/head.js
@@ -17,4 +17,33 @@ export default {
       content: '#2b2b2b'
     },
   ],
+  link: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      type: 'image/x-icon',
+      href: 'apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: 'favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: 'favicon-16x16.png',
+    },
+    {
+      rel: 'manifest',
+      href: 'site.webmanifest'
+    },
+    {
+      rel: 'mask-icon',
+      href: 'safari-pinned-tab.svg',
+      color: '#5bbad5'
+    }
+  ],
 }
